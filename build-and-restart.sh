292044f70279decb940ecc49ea6d320a51ee6309
Łukasz Żeligowski <l.zeligowski@grupait.com>
2016-09-19 14:21:02 +0200

    Better start - not so fast...

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -4,9 +4,13 @@ docker kill $(docker ps -q -a)
 docker rm $(docker ps -q -a)
 
 docker run -d --name t1 vertx-docker-test
+sleep 1s
 docker run -d --name t2 vertx-docker-test
+sleep 1s
 docker run -d --name t3 vertx-docker-test
+sleep 1s
 docker run -d --name t4 vertx-docker-test
+sleep 1s
 
 #docker service create --replicas 3 --name vertx-docker-test vertx-docker-test
 #docker service update --image vertx-docker-test:latest vertx-docker-test
